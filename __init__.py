b1ff3549eba89a60609193851100fd44398279c0
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2015-06-14 18:11:18 -0700

    clean up

--- __init__.py
+++ __init__.py
@@ -1,8 +1,7 @@
-import pandas as pd
 import requests
 import json
 
-class LendingClub:
+class LendingClubAPI:
     def __init__(self, investor_id: int, API_key: str) -> object:
         self.investor_id = investor_id
         self.API_key = API_key
@@ -10,11 +9,12 @@ class LendingClub:
        'Accept': 'application/json',
        'Content-type': 'application/json',
        'Authorization': API_key}
+        self.endpoint = "https://api.lendingclub.com/api/invstor/v1"
 
     def get_portfolios_owned(self) -> list:
         """List portofolios, returns response if failed"""
         r = requests.get(
-            "https://api.lendingclub.com/api/investor/v1/accounts/%d/portfolios" % investor_id,
+            "%s/accounts/%d/portfolios" % (self.endpoint, investor_id),
             headers=self.header)
 
         if not r.ok:
@@ -34,7 +34,7 @@ class LendingClub:
         }
 
         r = requests.post(
-            "https://api.lendingclub.com/api/investor/v1/accounts/%d/portfolios" % investor_id,
+            "%s/accounts/%d/portfolios" % (self.endpoint, investor_id),
             headers=self.header,
             data=json.dumps(portfolio_details))
         
@@ -47,7 +47,7 @@ class LendingClub:
     def get_detailed_notes_owned(self) -> dict:
         """Get my detailed notes"""
         r = requests.get(
-            "https://api.lendingclub.com/api/investor/v1/accounts/%d/detailednotes" % investor_id,
+            "%s/accounts/%d/detailednotes" % (self.endoint, investor_id),
             headers=self.header)
 
         if not r.ok:
@@ -95,18 +95,4 @@ class LendingClub:
             return r
 
         return True
-
- 
-# Login credentials.
-investor_id = 17403271
-API_key = 'PcKTg9ktpDih56gvu5TUARD92NM='
-
-lc = LendingClub(investor_id, API_key)
-
-notes = lc.get_listed_loans()
-df = pd.DataFrame.from_dict(notes)
-
-# summary
-#print df['loanStatus'].value_counts()
-#defaults = df[df['loanStatus'].isin(('Charged Off', 'Default'))]
 
