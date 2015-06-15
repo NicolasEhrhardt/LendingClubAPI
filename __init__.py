619957cd6a3ce5bb631e0d6063df11599aadac5b
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2015-06-14 19:32:42 -0700

    fix typos

--- __init__.py
+++ __init__.py
@@ -1,38 +1,40 @@
-import requests
 import json
 
+import requests
+
+
 class LendingClubAPI:
-    def __init__(self, investor_id: int, API_key: str):
+    def __init__(self, investor_id: int, api_key: str):
         self.investor_id = investor_id
-        self.API_key = API_key
+        self.API_key = api_key
         self.header = {
-       'Accept': 'application/json',
-       'Content-type': 'application/json',
-       'Authorization': API_key}
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+            'Authorization': api_key}
         self.endpoint = "https://api.lendingclub.com/api/invstor/v1"
 
     def get_portfolios_owned(self) -> list:
         """List portofolios, returns response if failed"""
         response = requests.get(
-            "%s/accounts/%d/portfolios" % (self.endpoint, investor_id),
+            "%s/accounts/%d/portfolios" % (self.endpoint, self.investor_id),
             headers=self.header)
-        
-        respnse.raise_for_status()
 
-        portfolios = respnse.json()['myPortfolios']
+        response.raise_for_status()
+
+        portfolios = response.json()['myPortfolios']
 
         return portfolios
 
     def create_portfolio(self, name: str, description: str) -> bool:
         """Create a portfolio, returns true if succedeed response if failed"""
         portfolio_details = {
-            'aid': investor_id,
+            'aid': self.investor_id,
             'portfolioName': name,
             'portfolioDescription': description,
         }
 
         response = requests.post(
-            "%s/accounts/%d/portfolios" % (self.endpoint, investor_id),
+            "%s/accounts/%d/portfolios" % (self.endpoint, self.investor_id),
             headers=self.header,
             data=json.dumps(portfolio_details))
 
@@ -43,9 +45,9 @@ class LendingClubAPI:
     def get_detailed_notes_owned(self) -> dict:
         """Get my detailed notes"""
         response = requests.get(
-            "%s/accounts/%d/detailednotes" % (self.endoint, investor_id),
+            "%s/accounts/%d/detailednotes" % (self.endpoint, self.investor_id),
             headers=self.header)
-        
+
         response.raise_for_status()
 
         notes = response.json()['myNotes']
@@ -68,20 +70,19 @@ class LendingClubAPI:
     def submit_order(self, loan_ids: list, portfolio_id: int, requested_amount: int=25) -> bool:
         """Place order for list of loanIds, investing"""
         orders = {
-            'aid': investor_id,
-            'orders': [{ 
-                'loanId': loan_id,
-                'requestedAmount': requested_amount,
-                'portfolioId': portfolio_id,
-            } for loanId in loanIds]
+            'aid': self.investor_id,
+            'orders': [{
+                           'loanId': loan_id,
+                           'requestedAmount': requested_amount,
+                           'portfolioId': portfolio_id,
+                       } for loan_id in loan_ids]
         }
 
         response = requests.post(
-            "https://api.lendingclub.com/api/investor/v1/accounts/%d/orders" % investor_id,
+            "https://api.lendingclub.com/api/investor/v1/accounts/%d/orders" % self.investor_id,
             headers=self.header,
             data=json.dumps(orders))
-        
+
         response.raise_for_status()
 
         return True
-
